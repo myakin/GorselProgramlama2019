bcce3dcbcab83dd091c46840d029cd27011aa672
Murat Yakin <yakin_murat@yahoo.com>
2019-11-02 15:36:52 +0300

    Ders 2 ortasinda

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIManager : MonoBehaviour
+{
+    public Text baslikText;
+    public Text solKolonOrtaText;
+    private int sayim;
+
+    private void Start() {
+        baslikText.text = "Merhaba";
+    }
+
+    private void Update() {
+        sayim++;
+        solKolonOrtaText.text = "Sayim: " + sayim;
+    }
+
+
+}
---
+++ Assets/Scripts/UIManagerDers2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManagerDers2 : MonoBehaviour
+{
+    public Button doktor1Button, doktor2Button, doktor3Button;
+    public Text uzmanlikAlaniText;
+    public Text mezuniyetYiliText;
+    public Text hastaneyeGirisTarihiText;
+    public Text hastaSayisiText;
+    public Text emekliligineKalanYilText;
+
+
+    private void Start() {
+        doktor1Button.onClick.AddListener(DoktorBilgisiniYazdir);
+    }
+
+    private void DoktorBilgisiniYazdir() {
+        uzmanlikAlaniText.text = "Kardiyoloji";
+        mezuniyetYiliText.text = "1991";
+        hastaneyeGirisTarihiText.text = "1992";
+        hastaSayisiText.text = "5";
+        emekliligineKalanYilText.text = "30";
+    }
+
+    private void DoktorBilgisiniYazdir2() {
+    }
+    private void DoktorBilgisiniYazdir3() {
+    }
+}
